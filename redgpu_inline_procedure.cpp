1fab7553629232e5a6048b43192363843eb878d8
Constantine Tarasenkov <iamvfx@gmail.com>
2021-08-29 13:11:54 +0300

    Initial commit

---
+++ redgpu_inline_procedure.cpp
@@ -0,0 +1,13 @@
+#include "redgpu_inline_procedure.h"
+
+REDGPU_DECLSPEC void REDGPU_API redCallSetInlineProcedure(RedHandleCalls calls, uint64_t inlineProcedureUniqueKey, const RedInlineProcedure * inlineProcedure, RedStatuses * outStatuses, const char * optionalFile, int optionalLine, void * optionalUserData) {
+
+}
+
+REDGPU_DECLSPEC void REDGPU_API redCallSetInlineProcedureCompute(RedHandleCalls calls, uint64_t inlineProcedureUniqueKey, const RedInlineProcedureCompute * inlineProcedure, RedStatuses * outStatuses, const char * optionalFile, int optionalLine, void * optionalUserData) {
+
+}
+
+REDGPU_DECLSPEC void REDGPU_API redBeforeDestroyCallsDestroyInlineProcedures(RedContext context, RedHandleGpu gpu, RedHandleCalls calls, const char * optionalFile, int optionalLine, void * optionalUserData) {
+
+}
---
+++ redgpu_inline_procedure.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#ifdef _WIN32
+#include "C:/RedGpuSDK/redgpu.h"
+#endif
+#ifdef __linux__
+#include "/opt/RedGpuSDK/redgpu.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct RedInlineProcedure {
+  RedContext                   context;
+  RedHandleGpu                 gpu;
+  const char *                 handleName;
+  RedHandleOutputDeclaration   outputDeclaration;
+  RedHandleProcedureParameters procedureParameters;
+  const char *                 gpuCodeVertexMainProcedureName;
+  const char *                 gpuCodeVertexHandleName;
+  uint64_t                     gpuCodeVertexIrBytesCount;
+  const void *                 gpuCodeVertexIr;
+  const char *                 gpuCodeFragmentMainProcedureName;
+  const char *                 gpuCodeFragmentHandleName;
+  uint64_t                     gpuCodeFragmentIrBytesCount;
+  const void *                 gpuCodeFragmentIr;
+  const RedProcedureState *    state;
+  const void *                 stateExtension;
+} RedInlineProcedure;
+
+typedef struct RedInlineProcedureCompute {
+  RedContext                   context;
+  RedHandleGpu                 gpu;
+  const char *                 handleName;
+  RedHandleProcedureParameters procedureParameters;
+  const char *                 gpuCodeMainProcedureName;
+  const char *                 gpuCodeHandleName;
+  uint64_t                     gpuCodeIrBytesCount;
+  const void *                 gpuCodeIr;
+} RedInlineProcedureCompute;
+
+REDGPU_DECLSPEC void REDGPU_API redCallSetInlineProcedure                    (RedHandleCalls calls, uint64_t inlineProcedureUniqueKey, const RedInlineProcedure * inlineProcedure, RedStatuses * outStatuses, const char * optionalFile, int optionalLine, void * optionalUserData);
+REDGPU_DECLSPEC void REDGPU_API redCallSetInlineProcedureCompute             (RedHandleCalls calls, uint64_t inlineProcedureUniqueKey, const RedInlineProcedureCompute * inlineProcedure, RedStatuses * outStatuses, const char * optionalFile, int optionalLine, void * optionalUserData);
+
+REDGPU_DECLSPEC void REDGPU_API redBeforeDestroyCallsDestroyInlineProcedures (RedContext context, RedHandleGpu gpu, RedHandleCalls calls, const char * optionalFile, int optionalLine, void * optionalUserData);
+
+#ifdef __cplusplus
+}
+#endif
